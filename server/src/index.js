dedc83599f1961e2509cbb205d63507566654def
Eugenio Pace <eugenio.pace@gmail.com>
2025-12-14 09:17:44 -0800

    Fixed rout for app

--- server/src/index.js
+++ server/src/index.js
@@ -58,7 +58,7 @@ if (process.env.NODE_ENV === 'production') {
 
   app.use(express.static(clientBuildPath));
 
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.join(clientBuildPath, 'index.html'));
   });
 }
